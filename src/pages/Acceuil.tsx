91d81e4f53a074d5ec734662122b3e9664551cf5
Garnilo <yemtegarnilo@gmail.com>
2026-03-08 20:52:32 +0100

    a good avancement. finished to make the home page. ready to fixe some little details and go to an other page

--- src/pages/Acceuil.tsx
+++ src/pages/Acceuil.tsx
@@ -1,6 +1,60 @@
+import johnDoe from "../assets/images/john-doe-about.jpg"
+import "./pages.css"
 function Home() {
+    const jdimage = johnDoe
     return(
-        <h1>HOME</h1>
+        <main>
+            <div className="fond">
+                <h1>Bonjour, je suis John Doe</h1>
+                <h2>Developpeur web full stack</h2>
+                <button className="btn btn-danger"> en savoir plus</button>
+            </div>
+            <div className="section-info">
+                <div className="a-propos">
+                    <h2>A propos</h2>
+                    <img src={jdimage} alt="john doe about" className="profil" />
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.Pellentesque ligula ipsum, tristique sed tortor vel, finibus congue ligula. Vivamus consectetur massa. Morbi id erat erat.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.Pellentesque ligula ipsum, tristique sed tortor vel, finibus congue ligula. Vivamus consectetur massa. Morbi id erat erat.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.Pellentesque ligula ipsum, tristique sed tortor vel, finibus congue ligula. Vivamus consectetur massa. Morbi id erat erat.
+                    </p>
+                </div>
+                <div className="mes-competences">
+                    <h2>Mes compétences</h2>
+                    <ul className="competence-list list-unstyled">
+                        <li>HTML5 90%
+                            <div className="progress" role="progressbar" aria-label="Danger example" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100}>
+                                <div className="progress-bar bg-danger" style={{ width: '90%' }}></div>
+                            </div>
+                        </li>
+                        <li>CSS3 80%
+                            <div className="progress" role="progressbar" aria-label="Info example" aria-valuenow={80} aria-valuemin={0} aria-valuemax={100}>
+                                <div className="progress-bar bg-info" style={{ width: '80%' }}></div>
+                            </div>
+                        </li>
+                        <li>JAVASCRIPT 75%
+                            <div className="progress" role="progressbar" aria-label="Warning example" aria-valuenow={70} aria-valuemin={0} aria-valuemax={100}>
+                                <div className="progress-bar bg-warning" style={{ width: '70%' }}></div>
+                            </div>
+                        </li>
+                        <li>PHP 70%
+                            <div className="progress" role="progressbar" aria-label="Success example" aria-valuenow={60} aria-valuemin={0} aria-valuemax={100}>
+                                <div className="progress-bar bg-success" style={{ width: '60%' }}></div>
+                            </div>
+                        </li>
+                        <li>REACT 65%
+                            <div className="progress" role="progressbar" aria-label="Default example" aria-valuenow={50} aria-valuemin={0} aria-valuemax={100}>
+                                <div className="progress-bar bg-default" style={{ width: '50%' }}></div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </main>
     )
 }
 
